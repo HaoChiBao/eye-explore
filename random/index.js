41b41c3750ae09e787e25899684628444a64595e
James Yang <jamesyang663@gmail.com>
2023-01-21 12:22:50 -0500

    key removed

--- random/index.js
+++ random/index.js
@@ -3,7 +3,7 @@ fetch('https://vip-face.cognitiveservices.azure.com//face/v1.0/detect', {
     endpoint: 'https://vip-face.cognitiveservices.azure.com/',
     headers: {
         'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': 'a5107ef5efc04b07a66c3fce4c4ea594',
+        'Ocp-Apim-Subscription-Key': '',
     },
     body: JSON.stringify({
         // url: "https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg"
