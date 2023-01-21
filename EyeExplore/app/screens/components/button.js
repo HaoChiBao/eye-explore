7821d35583929e93d5a32268510fc99e1ccb1d73
Judy Zhou <dzhou224@uwo.ca>
2023-01-21 16:39:56 -0500

    button.... NO I WANNA DIE

--- EyeExplore/app/screens/components/button.js
+++ EyeExplore/app/screens/components/button.js
@@ -1,13 +1,47 @@
 import * as React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 
-export default function Button({ title, onPress, icon, color }) {
+export default function Button({ title, onPress, icon }) {
     return (
-        <TouchableOpacity onPress={onPress} style={styles.button}>
-            <Entypo name={icon} size={28} color={color ? color : '#f1f1f1'} />
-            <Text styles={styles.text}>{title}</Text>
-        </TouchableOpacity>
+        <View style={styles.container}>
+            <TouchableOpacity onPress={onPress} style={styles.button}>
+                <Entypo name={icon} size={40} color={"#F2F3FF"} />
+                {/* <Text styles={styles.text}>{title}</Text> */}
+            </TouchableOpacity>
+        </View >
     )
 
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+
+    },
+
+    button: {
+        height: 80,
+        width: 250,
+        backgroundColor: "#40376E",
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+        padding: 10,
+        borderRadius: 39,
+    },
+
+    text: {
+        fontWeight: 'bold',
+        fontSize: 80,
+        color: '#F2F3FF',
+        marginLeft: 10,
+    }
+}
+)
