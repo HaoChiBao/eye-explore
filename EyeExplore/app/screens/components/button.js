3e09b120b3716032aea78759de65287e2ba5b91c
Judy Zhou <dzhou224@uwo.ca>
2023-01-21 12:47:23 -0500

    button!!!

--- EyeExplore/app/screens/components/button.js
+++ EyeExplore/app/screens/components/button.js
@@ -1,2 +1,13 @@
 import * as React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+export default function Button({ title, onPress, icon, color }) {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+            <Entypo name={icon} size={28} color={color ? color : '#f1f1f1'} />
+            <Text styles={styles.text}>{title}</Text>
+        </TouchableOpacity>
+    )
+
+}
